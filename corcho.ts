66553e2ca8c8653fe0c07fde4cddec91cf3c00b0
ManuelBanchero <manuelbanchero04@gmail.com>
2025-11-10 19:15:50 -0300

    ej corcho arreglado

--- corcho.ts
+++ corcho.ts
@@ -7,24 +7,24 @@
     - El juego se representa por un árbol (en nuestro ejemplo, un grafo) cuyos vértices están etiquetados con el número actual de corchos que quedan.
 */
 
-
 function corcho(corchos: number, jugador: number = 1): number {
     if (corchos === 0) return jugador
 
     let sig = 1
-    let val = -1 * jugador
+    let val = jugador * -1
     let poda = false
 
     while (sig <= 3 && sig <= corchos && !poda) {
-        corchos = corchos - sig
+        corchos -= sig
+        if (jugador === 1)
+            val = Math.max(val, corcho(corchos, jugador * -1))
+        else
+            val = Math.min(val, corcho(corchos, jugador * -1))
 
-        if (jugador === 1) val = Math.max(corcho(corchos, -1 * jugador))
-        else val = Math.min(corcho(corchos, -1 * jugador))
+        if (jugador * val === 1) poda = true
+        else corchos += sig
 
-        if (corchos * val === 1) poda = true
-        else corchos = corchos + sig // Estado de corchos anterior
-
-        sig += 1
+        sig++
     }
 
     return val
